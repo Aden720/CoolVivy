4ff7bd3e32a65d58e84e10f42b98171593374501
Aden720 <37283564+Aden720@users.noreply.github.com>
2024-10-16 13:34:05 +0000

    spotify multi artists string

--- main.py
+++ main.py
@@ -134,19 +134,33 @@ def getSpotifyParts(embed):
     if parts[1] == 'Single' or parts[1] == 'Album':
         attributes = ['Artist', 'Type', 'Released']
         if len(parts) > 3 and parts[1] != 'Single':
-            parts[1] = f'{parts[1]} - {parts[3]}' #Type - Track num
+            parts[1] = f'{parts[1]} - {parts[3]}'  #Type - Track num
             parts.pop(3)
     elif parts[0] == 'Playlist':
         attributes = ['Artist', 'Type', 'Saves :green_heart:']
         parts[0], parts[1] = parts[1], parts[0]
         if len(parts) > 3:
-            parts[1] = f'{parts[1]} - {parts[2]}' #Playlist - num items
+            parts[1] = f'{parts[1]} - {parts[2]}'  #Playlist - num items
             parts.pop(2)
     else:
         attributes = ['Artist', 'title', 'Type', 'Released']
-        
+
     for index, attribute in enumerate(attributes):
         spotifyParts[f'{attribute}'] = parts[index]
+
+    artist_parts_comma = spotifyParts['Artist'].split(', ')
+    if len(artist_parts_comma) > 1:
+        spotifyParts['Artists'] = spotifyParts['Artist']
+        spotifyParts.pop('Artist', None)
+
+    # Reorder keys: 'Type' and 'Released' should follow 'Artists' or 'Artist'
+    if 'Artists' in spotifyParts:
+        order = ['Artists'] + [key for key in spotifyParts if key != 'Artists']
+        spotifyParts = {
+            key: spotifyParts[key]
+            for key in order if key in spotifyParts
+        }
+
     return spotifyParts
 
 
