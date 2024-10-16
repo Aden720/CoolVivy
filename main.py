c5f8ba6d2f3823d35177058bb5cf0d2bd67079d3
Aden720 <37283564+Aden720@users.noreply.github.com>
2024-10-16 15:11:40 +0000

    move spotify method to separate file

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from ytmusicapi import YTMusic
 from bandcamp_utils import getBandcampParts
 from general_utils import formatMillisecondsToDurationString, formatTimeToDisplay
 from soundcloud_utils import getSoundcloudParts
+from spotify_utils import getSpotifyParts
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -125,44 +126,6 @@ def formatTimeToTimestamp(time):
     timeString, timezoneOffset = timezoneString.split(
         '-') if '-' in timezoneString else timezoneString.split('+')
     return dateString + 'T' + timeString
-
-
-def getSpotifyParts(embed):
-    spotifyParts = {'embedPlatformType': 'spotify'}
-    attributes = ['Artist', 'Type', 'Released']
-    parts = embed.description.split(' · ')
-    if parts[1] == 'Single' or parts[1] == 'Album':
-        attributes = ['Artist', 'Type', 'Released']
-        if len(parts) > 3 and parts[1] != 'Single':
-            parts[1] = f'{parts[1]} - {parts[3]}'  #Type - Track num
-            parts.pop(3)
-    elif parts[0] == 'Playlist':
-        attributes = ['Artist', 'Type', 'Saves :green_heart:']
-        parts[0], parts[1] = parts[1], parts[0]
-        if len(parts) > 3:
-            parts[1] = f'{parts[1]} - {parts[2]}'  #Playlist - num items
-            parts.pop(2)
-    else:
-        attributes = ['Artist', 'title', 'Type', 'Released']
-
-    for index, attribute in enumerate(attributes):
-        spotifyParts[f'{attribute}'] = parts[index]
-
-    artist_parts_comma = spotifyParts['Artist'].split(', ')
-    if len(artist_parts_comma) > 1:
-        spotifyParts['Artists'] = spotifyParts['Artist']
-        spotifyParts.pop('Artist', None)
-
-    # Reorder keys: 'Type' and 'Released' should follow 'Artists' or 'Artist'
-    if 'Artists' in spotifyParts:
-        order = ['Artists'] + [key for key in spotifyParts if key != 'Artists']
-        spotifyParts = {
-            key: spotifyParts[key]
-            for key in order if key in spotifyParts
-        }
-
-    return spotifyParts
-
 
 #Check if it's a Youtube Music track based on track type
 def isYoutubeMusic(type):
