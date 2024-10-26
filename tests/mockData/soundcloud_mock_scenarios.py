0a6756fa80dbfa885e5649afd26acc77285dc71c
Aden720 <37283564+Aden720@users.noreply.github.com>
2024-10-26 20:48:10 +0000

    unit test basic soundcloud embed

--- tests/mockData/soundcloud_mock_scenarios.py
+++ tests/mockData/soundcloud_mock_scenarios.py
@@ -3,8 +3,36 @@ from unittest.mock import MagicMock
 from sclib import Track
 
 
+def setupBasicEmbed():
+    embed = MagicMock()
+    embed.url = 'https://soundcloud.com/someTrack'
+    return embed
+
+
 def setupBasicTrack():
     mock_track = MagicMock(spec=Track)
     mock_track.artist = 'Mock Artist'
     mock_track.title = 'Mock Track Title'
+    mock_track.genre = ''
+    mock_track.duration = 123456
+    mock_track.created_at = '2022-01-01T00:00:00Z'
+    mock_track.likes_count = 123
+    mock_track.playback_count = 456
+    mock_track.publisher_metadata = {}
+    mock_track.tag_list = ''
+    mock_track.purchase_title = ''
+    mock_track.purchase_url = ''
+    mock_track.user = {}
+    return mock_track
+
+
+def setupBasicTrackWithLikes():
+    mock_track = setupBasicTrack()
+    mock_track.likes_count = 100
+    return mock_track
+
+
+def setupBasicTrackWithTags():
+    mock_track = setupBasicTrack()
+    mock_track.tag_list = '"Mock Tag1", "Mock Tag2"'
     return mock_track
--- tests/test_soundcloud_utils.py
+++ tests/test_soundcloud_utils.py
@@ -2,10 +2,10 @@
 import unittest
 from unittest.mock import MagicMock, patch
 
-from mockData.soundcloud_mock_scenarios import setupBasicTrack
+from mockData.soundcloud_mock_scenarios import setupBasicEmbed, setupBasicTrack
 from sclib import Track
 
-from soundcloud_utils import fetchTrack, split_tags
+from soundcloud_utils import fetchTrack, getSoundcloudParts, split_tags
 
 
 class TestSoundcloudUtils(unittest.TestCase):
@@ -68,9 +68,57 @@ class TestSoundcloudUtils(unittest.TestCase):
         # Call the fetchTrack function
         result = fetchTrack(mock_track_url)
         # Assertions
-        self.assertEqual(result.title, 'Mock Track Title')
-        self.assertEqual(result.artist, 'Mock Artist')
-        self.assertEqual(result.title, 'Mock Track Title')
-        mock_requests_get.assert_not_called()
-        mock_soundcloud_api.return_value.resolve.assert_called_once_with(
+        if isinstance(result, Track):
+            self.assertEqual(result.title, 'Mock Track Title')
+            self.assertEqual(result.artist, 'Mock Artist')
+            mock_requests_get.assert_not_called()
+            mock_soundcloud_api.return_value.resolve.assert_called_once_with(
+                'https://soundcloud.com/someTrack')
+        else:
+            self.fail("This test is intentionally failing.")
+
+    @patch('soundcloud_utils.requests.get')
+    @patch('soundcloud_utils.SoundcloudAPI')
+    def test_fetchTrack_http_error(self, mock_soundcloud_api,
+                                   mock_requests_get):
+        # Simulate the HTTP response from the initial SoundCloud URL
+        mock_response = MagicMock()
+        mock_response.status_code = 404
+        mock_requests_get.return_value = mock_response
+        # URL to test
+        mock_track_url = 'https://on.soundcloud.com/auqhgASa'
+
+        # Assertions
+        with self.assertRaises(Exception) as e:
+            # Call the fetchTrack function
+            fetchTrack(mock_track_url)
+        self.assertEqual(str(e.exception),
+                         "Unable to fetch Soundcloud Mobile URL")
+        mock_requests_get.assert_called_once_with(mock_track_url)
+        mock_soundcloud_api.return_value.resolve.assert_not_called()
+
+    @patch('soundcloud_utils.fetchTrack')
+    def test_getSoundcloudParts(self, mock_fetch_track):
+        # Arrange
+        mock_track = setupBasicTrack()
+        mock_fetch_track.return_value = mock_track
+
+        # Act
+        result = getSoundcloudParts(setupBasicEmbed())
+        print(result)
+
+        # Assert
+        expected_parts = {
+            'embedPlatformType': 'soundcloud',
+            'embedColour': 0xff5500,
+            'title': f'{mock_track.artist} - {mock_track.title}',
+            'Duration': '`2:03`',
+            'Uploaded on': '1 January 2022',
+            'Likes': ':orange_heart: 123',
+            'Plays': ':notes: 456'
+        }
+        print(expected_parts)
+        assert result == expected_parts
+
+        mock_fetch_track.assert_called_once_with(
             'https://soundcloud.com/someTrack')
