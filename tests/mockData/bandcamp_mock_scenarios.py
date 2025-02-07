7a6714b9230d114b6c1644b26c056c16730dff2a
Aden720 <37283564+Aden720@users.noreply.github.com>
2025-02-07 20:44:10 +0000

    Assistant checkpoint: Refactored bandcamp mock scenarios with MagicMock
    Assistant generated file changes:
    - tests/mockData/bandcamp_mock_scenarios.py: Refactor using MagicMock and reduce duplication
    
    ---
    
    User prompt:
    
    can you refactor bandcamp mock scenarios file to use magicmock and reduce the code duplication

--- tests/mockData/bandcamp_mock_scenarios.py
+++ tests/mockData/bandcamp_mock_scenarios.py
@@ -1,34 +1,33 @@
-class Embed:
 
-  def __init__(self):
-    self.url = ''
-    self.title = ''
-    self.provider = {}
+from unittest.mock import MagicMock
+
+
+def create_base_embed():
+    embed = MagicMock()
+    embed.url = ''
+    embed.title = ''
+    embed.provider = MagicMock()
+    embed.provider.name = 'Artist Name'
+    embed.provider.url = 'https://artist.bandcamp.com'
+    return embed
 
 
 def setupBasicEmbed():
-  embed = Embed()
-  embed.url = 'https://artist.bandcamp.com/track/song-title'
-  embed.title = 'Song Title, by Artist Name'
-  embed.provider.name = 'Artist Name'
-  embed.provider.url = 'https://artist.bandcamp.com'
-  embed.description = 'from the album Album Name'
-  return embed
+    embed = create_base_embed()
+    embed.url = 'https://artist.bandcamp.com/track/song-title'
+    embed.title = 'Song Title, by Artist Name'
+    embed.description = 'from the album Album Name'
+    return embed
 
 
 def setupAlbumEmbed():
-  embed = type('', (), {})()
-  embed.url = 'https://artist.bandcamp.com/album/album-title'
-  embed.title = 'Album Title, by Artist Name'
-  embed.provider = type('', (), {})()
-  embed.provider.name = 'Artist Name'
-  embed.provider.url = 'https://artist.bandcamp.com'
-  return embed
+    embed = create_base_embed()
+    embed.url = 'https://artist.bandcamp.com/album/album-title'
+    embed.title = 'Album Title, by Artist Name'
+    return embed
 
 
 def setupDiscographyEmbed():
-  embed = type('', (), {})()
-  embed.url = 'https://artist.bandcamp.com/music'
-  embed.provider = type('', (), {})()
-  embed.provider.name = 'Artist Name'
-  return embed
+    embed = create_base_embed()
+    embed.url = 'https://artist.bandcamp.com/music'
+    return embed
