172deff067662dbc65dc813ed20c79cdcee13112
Aden720 <37283564+Aden720@users.noreply.github.com>
2025-02-15 12:19:20 +0000

    spotify map album

--- spotify_utils.py
+++ spotify_utils.py
@@ -27,37 +27,21 @@ def getSpotifyParts(embed):
             title = track['name']
             artists = track['artists']
             album = track['album']
-            releaseDate = album['release_date']
-            releaseDateFormat = album['release_date_precision']
 
             #duration
             spotifyParts['Duration'] = (formatMillisecondsToDurationString(
                 track["duration_ms"]))
 
             #Released on
-            if releaseDateFormat == 'day':
-                releaseDateString = formatTimeToDisplay(
-                    releaseDate, '%Y-%m-%d')
-            elif releaseDateFormat == 'month':
-                releaseDateString = formatTimeToDisplay(
-                    releaseDate, '%Y-%m', '%B %Y')
-            else:
-                releaseDateString = releaseDate
-            spotifyParts['Released'] = releaseDateString
+            spotifyParts['Released'] = getReleaseDateString(album)
 
             #thumbnail
             if len(album['images']) > 0:
                 spotifyParts['thumbnailUrl'] = album['images'][0]['url']
 
             #artist
-            artistString = ', '.join([
-                f'[{artist["name"]}]({artist["external_urls"]["spotify"]})'
-                for artist in artists
-            ])
-            titleArtists = ', '.join([
-                artist['name'] for artist in artists
-                if artist['name'] not in title
-            ])
+            artistString = getFormattedArtistString(artists)
+            titleArtists = getFormattedTitleArtistString(artists, title)
             if len(artists) > 1:
                 spotifyParts['Artists'] = artistString
             else:
@@ -70,14 +54,82 @@ def getSpotifyParts(embed):
                 {album["external_urls"]["spotify"]})')
 
             #title
-            remixRegex = r"(.+?)\s[-–]\s(.*?(Remix|Mix|Edit).*)"
-            if re.match(remixRegex, title):
-                title = re.sub(remixRegex, r'\1 (\2)', title)
+            title = reformatTitle(title)
             spotifyParts['title'] = (f'{titleArtists} - {title}'
                                      if titleArtists else title)
         elif embed.url.startswith('https://open.spotify.com/album'):
-            raise Exception('bypassing until mapping is complete')
             album = sp.album(embed.url)
+            if not album:
+                raise Exception('Spotify album data not found')
+            #aliases
+            title = album['name']
+            artists = album['artists']
+            tracks = album['tracks']['items']
+            totalTracks = album['total_tracks']
+
+            artistString = getFormattedArtistString(artists)
+            titleArtists = getFormattedTitleArtistString(artists, title)
+
+            trackStrings = []
+            trackSummaryCharLength = 0
+            totalDuration = 0
+            for track in tracks:
+                totalDuration += track['duration_ms']
+                trackTitle = reformatTitle(track['name'])
+                trackArtists = getFormattedTitleArtistString(
+                    track['artists'], trackTitle)
+                trackString = (
+                    '1. ' +
+                    (f'[{trackArtists} - {trackTitle}]' if trackArtists
+                     and trackArtists != titleArtists else f'[{trackTitle}]') +
+                    f'({track["external_urls"]["spotify"]})' +
+                    f' `{formatMillisecondsToDurationString(track["duration_ms"])}`'
+                )
+                trackStringLength = len(trackString) + 1
+                if trackSummaryCharLength + trackStringLength <= 1000:
+                    trackStrings.append(trackString)
+                    trackSummaryCharLength += trackStringLength
+
+            spotifyParts['Duration'] = (
+                formatMillisecondsToDurationString(totalDuration))
+
+            #Artist
+            if len(artists) > 1:
+                spotifyParts['Artists'] = artistString
+            elif artists[0]['name'] == 'Various Artists':
+                spotifyParts['Artists'] = 'Various Artists'
+            else:
+                spotifyParts['Artist'] = artistString
+
+            #Label
+            if album.get('label'):
+                spotifyParts['Label'] = album['label']
+
+            #thumbnail
+            if len(album['images']) > 0:
+                spotifyParts['thumbnailUrl'] = album['images'][0]['url']
+
+            #Description
+            if totalTracks > 1:
+                spotifyParts['description'] = f'{album["total_tracks"]} \
+                track album'
+
+            else:
+                spotifyParts['Duration'] = (formatMillisecondsToDurationString(
+                    tracks[0]["duration_ms"]))
+
+            #Released on
+            spotifyParts['Released'] = getReleaseDateString(album)
+
+            spotifyParts['Tracks'] = '\n'.join(trackStrings)
+            if len(trackStrings) != totalTracks:
+                spotifyParts[
+                    'Tracks'] += f'\n...and {totalTracks - len(trackStrings)} more'
+
+            #title
+            spotifyParts['title'] = (f'{titleArtists} - {title}'
+                                     if titleArtists and titleArtists != 'Various Artists' else title)
+
         elif embed.url.startswith('https://open.spotify.com/playlist'):
             raise Exception('bypassing until mapping is complete')
             playlist = sp.playlist(embed.url)
@@ -121,3 +173,34 @@ def getSpotifyParts(embed):
             }
 
     return spotifyParts
+
+
+def getReleaseDateString(album):
+    releaseDate = album['release_date']
+    releaseDateFormat = album['release_date_precision']
+    if releaseDateFormat == 'day':
+        releaseDateString = formatTimeToDisplay(releaseDate, '%Y-%m-%d')
+    elif releaseDateFormat == 'month':
+        releaseDateString = formatTimeToDisplay(releaseDate, '%Y-%m', '%B %Y')
+    else:
+        releaseDateString = releaseDate
+    return releaseDateString
+
+
+def getFormattedArtistString(artists):
+    return ', '.join([
+        f'[{artist["name"]}]({artist["external_urls"]["spotify"]})'
+        for artist in artists
+    ])
+
+
+def getFormattedTitleArtistString(artists, title):
+    return ', '.join(
+        [artist['name'] for artist in artists if artist['name'] not in title])
+
+
+def reformatTitle(title):
+    remixRegex = r"(.+?)\s[-–]\s(.*?(Remix|Mix|Edit).*)"
+    if re.match(remixRegex, title):
+        title = re.sub(remixRegex, r'\1 (\2)', title)
+    return title
