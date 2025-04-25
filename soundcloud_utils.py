047639aba42d4efd946712e84e52e590aeeb6d9f
Aden720 <37283564+Aden720@users.noreply.github.com>
2025-04-25 02:11:32 +0000

    change download flag to be in description

--- soundcloud_utils.py
+++ soundcloud_utils.py
@@ -83,8 +83,10 @@ def getSoundcloudParts(embed):
                 f'{":arrow_down:" if isDownload else ":link:"} '
                 f'[{buyLinkName}]({track.purchase_url})')
         elif track.downloadable and track.has_downloads_left:
-            soundcloudParts[':arrow_down: File download available'] = (
-                f'<{track.download_url}>' if track.download_url else ' ')
+            soundcloudParts['description'] = (
+                ':arrow_down: File download available' +
+                (f'[here](<{track.download_url}>)'
+                 if track.download_url else ''))
 
         #Channel
         if track.user:
